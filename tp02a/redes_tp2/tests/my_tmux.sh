3feb12157b27e102e8da372b2e0f966fc2086f58
Rômulo Silva <romulo.rds89@gmail.com>
2020-03-21 21:37:45 -0300

    updates folder
    updates folder

--- tp02a/redes_tp2/tests/my_tmux.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -eu
-
-exe="python3.5 ../my_router.py"
-
-for i in $(seq 1 6) ; do
-    tmux split-pane -v $exe --addr "127.0.1.$i" --update-period 5 --startup-commands "$i.txt" &
-    tmux select-layout even-vertical
-done
